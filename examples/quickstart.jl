597a65d4e094e742bb30294f791ba65ae109396d
yutong huang <yutongh3@andrew.cmu.edu>
2024-12-02 21:36:12 -0500

    csv

--- examples/quickstart.jl
+++ examples/quickstart.jl
@@ -69,98 +69,33 @@ plot!(1:N, [x[4] for x in X], label="theta_dot")
 
 # on a separate plot, plot control over time
 plot2 = plot(1:N-1, [u[1] for u in U], label="Control")
-
-
-# solve with ilqr
-ilqr = Altro.iLQRSolver(prob, opts)
-initial_controls!(ilqr, U0)
-solve!(ilqr)
-
-X = states(ilqr)
-U = controls(ilqr)
-
-#plot position over time and volecity over time
-plot3 = plot(1:N, [x[1] for x in X], label="p")
-plot!(1:N, [x[2] for x in X], label="theta")
-plot!(1:N, [x[3] for x in X], label="p_dot")
-plot!(1:N, [x[4] for x in X], label="theta_dot")
-
-# on a separate plot, plot control over time
-plot4 = plot(1:N-1, [u[1] for u in U], label="Control")
-
 display(plot1)
 display(plot2)
-display(plot3)
-display(plot4)
 
-# using Ipopt
-# using MathOptInterface
-# const MOI = MathOptInterface
+#save x and u into a csv file
+using CSV
+using DataFrames
+CSV.write("x.csv", DataFrame(X, :auto))
+CSV.write("u.csv", DataFrame(U, :auto))
 
-# # Copy problem to avoid modifying the original problem
-# # prob_nlp = copy(prob)
-# prob_nlp = Problem(model, obj, x0, tf, xf=xf, constraints=conSet)
 
-# # # Add the dynamics and initial conditions as explicit constraints
-# TrajectoryOptimization.add_dynamics_constraints!(prob_nlp)
+# # solve with ilqr
+# ilqr = Altro.iLQRSolver(prob, opts)
+# initial_controls!(ilqr, U0)
+# solve!(ilqr)
 
-# # Reset our initial guess
-# initial_controls!(prob_nlp, U0)
-# rollout!(prob_nlp)
+# X = states(ilqr)
+# U = controls(ilqr)
 
-# Create the NLP
-nlp = TrajOptNLP(prob_nlp, remove_bounds=true, jac_type=:vector);
+# #plot position over time and volecity over time
+# plot3 = plot(1:N, [x[1] for x in X], label="p")
+# plot!(1:N, [x[2] for x in X], label="theta")
+# plot!(1:N, [x[3] for x in X], label="p_dot")
+# plot!(1:N, [x[4] for x in X], label="theta_dot")
 
-# optimizer = Ipopt.Optimizer()
-# TrajectoryOptimization.build_MOI!(nlp, optimizer)
-# MOI.optimize!(optimizer)
-# MOI.get(optimizer, MOI.TerminationStatus())
+# # on a separate plot, plot control over time
+# plot4 = plot(1:N-1, [u[1] for u in U], label="Control")
 
-# println("max_violation: ", max_violation(nlp))
-# println("cost:          ", cost(nlp));
 
-# using BenchmarkTools
-
-# # Reset initial guess and then benchmark ALTRO solver
-# initial_controls!(altro, U0)
-# b_altro = benchmark_solve!(altro)
-
-# # Reset initial guess and benchmark Ipopt solver
-# initial_controls!(prob_nlp, U0)
-# rollout!(prob_nlp)
-# nlp = TrajOptNLP(prob_nlp, remove_bounds=true, jac_type=:vector)
-# Z0 = copy(TrajectoryOptimization.get_trajectory(nlp).Z)
-
-# optimizer = Ipopt.Optimizer(print_level=0)
-# TrajectoryOptimization.build_MOI!(nlp, optimizer)
-# Z = MOI.VariableIndex.(1:length(Z0))
-# b_ipopt = @benchmark begin
-#     MOI.optimize!($optimizer)
-#     MOI.set($optimizer, MOI.VariablePrimalStart(), $Z, $Z0)
-#     MOI.get($optimizer, MOI.TerminationStatus())
-# end
-
-# using Statistics
-# @show cost(nlp)
-# @show cost(altro)
-# @show max_violation(nlp)
-# @show max_violation(altro)
-# jdg = judge(median(b_altro), median(b_ipopt))
-# println("Speed improvement: ", round(1/ratio(jdg).time), "x")
-# jdg
-
-# using TrajOptPlots
-# using MeshCat
-# using Plots
-
-# vis = Visualizer()
-# render(vis)
-
-# TrajOptPlots.set_mesh!(vis, model)
-# visualize!(vis, altro);
-# # visualize!(vis, model, TrajectoryOptimization.get_trajectory(nlp));
-# # visualize!(vis, altro, nlp);
-
-# visualize!(vis, model, get_trajectory(altro), get_trajectory(nlp));
-
-# delete!(vis["robot_copies"]);
+# display(plot3)
+# display(plot4)
